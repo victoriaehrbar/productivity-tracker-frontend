6fd5ef218d69eb67564ec6841b3444ba84eb7d40
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-01 20:42:36 -0600

    index.js update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 import App from './App';
 
