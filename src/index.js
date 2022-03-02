36c8642c01dd664fa73efd3da2ce78c40b518745
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-01 21:37:44 -0600

    import update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
+import monthReducer from './reducers/monthReducer'
 
 
 import App from './App';
