fe8421d8bdf679d2bc48e30ffa2bf82de3aacb5f
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 10:17:53 -0600

    month input state

--- src/components/MonthInput.js
+++ src/components/MonthInput.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 class MonthInput extends React.Component {
 
+    state = {
+        name: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render(){
         return(
             <div>
