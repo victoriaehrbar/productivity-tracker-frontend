15b7d1652dfd617cc0a608928c81f22df1daf2bf
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 16:04:45 -0600

    month singular componentinformation

---
+++ src/components/Month.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Redirect} from 'react-router-dom'
+import MonthEdit from './MonthEdit'
+
+import HabitsContainer from '../containers/HabitsContainer'
+
+const Month = (props) => {
+
+  console.log(props)
+  let month = props.months.filter(month => month.id == props.match.params.id)[0]
+
+  console.log(month)
+  return (
+
+    <div>
+      <h2>
+        {month ? month.name : null}
+      </h2>
+      <TransactionsContainer month={month}/><br/>
+      <h4>Edit Month</h4>
+      <MonthEdit month={month}/>
+    </div>
+  )
+
+
+}
+
+export default Month
