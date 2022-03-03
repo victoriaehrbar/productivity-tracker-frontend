4b7008bb1960611e2a1127ea3548201fc05aebee
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 16:30:20 -0600

    add habit action

---
+++ src/actions/addHabit.js
@@ -0,0 +1,21 @@
+export const addHabit = (habit, monthId) => {
+
+    return (dispatch) => {
+      fetch(`http://localhost:3000/api/v1/months/${monthId}/habits`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(transaction)
+      })
+      .then(response => response.json())
+      .then(month => {
+          if (month.error) {
+            alert(month.error)
+          } else {
+            dispatch({type: 'ADD_HABIT', payload: month})
+          }
+        }
+      )
+    }
+  }
