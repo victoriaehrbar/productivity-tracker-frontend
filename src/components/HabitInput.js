c016786ec63b22d5a91c4b1f75c49fe11d60173f
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 16:19:09 -0600

    habit input

---
+++ src/components/HabitInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {addHabit} from '../actions/addHabit'
+
+class HabitInput extends React.Component {
+
+  state = {
+    completed: '',
+    date: '',
+    description ''
+  }
