b3064339ede3168708bb406b80ecd79d33690d3a
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 09:33:48 -0600

    typo in fetch months

--- src/actions/fetchMonths.js
+++ src/actions/fetchMonths.js
@@ -2,7 +2,7 @@ export function fetchMonths() {
     return (dispatch) => {
       fetch('http://localhost:3000/api/v1/months')
       .then(resp => resp.json())
-      .then(accounts => dispatch({
+      .then(months => dispatch({
         type: 'FETCH_MONTHS',
         payload: months
       }))
