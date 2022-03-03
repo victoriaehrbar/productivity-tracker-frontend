0dc3c2ff903ddb99d3bcc757bdb6e669678d40ed
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 16:22:50 -0600

    Habit input form

--- src/components/HabitInput.js
+++ src/components/HabitInput.js
@@ -9,3 +9,43 @@ class HabitInput extends React.Component {
     date: '',
     description: ''
   }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addHabit(this.state, this.props.month.id)
+    this.setState({
+        completed: '',
+        date: '',
+        description: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Completed:</label>
+          <select name="completed" value={this.state.completed} onChange={this.handleChange}>
+            <option>true</option>
+            <option>false</option>
+          </select>
+          <label>Date:</label>
+          <input type="text" name="date" value={this.state.date} onChange={this.handleChange}/>
+          <label>Description:</label>
+          <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
+          <input type="submit"/>
+        </form>
+      </div>
+    )
+
+  }
+}
+
+
+export default connect(null, {addHabit})(HabitInput)
