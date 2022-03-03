d094a3769dea5398a3c6837be68dba8c476cca60
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 09:28:54 -0600

    months container update import

--- src/containers/MonthsContainer.js
+++ src/containers/MonthsContainer.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+import Months from '../components/Months'
+import MonthInput from '../components/MonthInput'
 
 class MonthsContainer extends React.Component {
 
     render(){
         return(
             <div>
-                MonthsContainer
+                <MonthInput/>
+                <Months/>
             </div>
 
         )
