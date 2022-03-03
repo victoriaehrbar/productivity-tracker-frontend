467f5cadf0180f644c22389ecb8d22f740b67e7c
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 09:51:56 -0600

    monthReducer

--- src/reducers/monthReducer.js
+++ src/reducers/monthReducer.js
@@ -1,6 +1,37 @@
-
-export default function monthReducer(state = {accounts: []}, action) {
-
-    return state
-
-}
+export default function accountReducer(state = {months: []}, action) {
+    switch (action.type) {
+      case 'FETCH_MONTHS':
+        return {months: action.payload}
+      case 'ADD_MONTH':
+        return {...state, months: [...state.months, action.payload]}
+      case 'ADD_HABIT':
+        let months = state.months.map(month => {
+          if (month.id === action.payload.id) {
+            return action.payload
+          } else {
+            return month
+          }
+        })
+        return {...state, months: months}
+      case 'DELETE_HABIT':
+        let monthsTwo = state.months.map(month => {
+          if (month.id === action.payload.id) {
+            return action.payload
+          } else {
+            return month
+          }
+        })
+        return {...state, accounts: accountsTwo}
+      case 'EDIT_MONTH':
+        let monthsThree = state.months.map(month => {
+          if (month.id === action.payload.id) {
+            return action.payload
+          } else {
+            return month
+          }
+        })
+        return {...state, months: monthsThree}
+      default:
+        return state
+    }
+  }
