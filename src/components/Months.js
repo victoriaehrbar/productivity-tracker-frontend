61589d4924f3193bc048f3ed00b3e6ef8c06d72c
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-03 09:22:03 -0600

    months link to

--- src/components/Months.js
+++ src/components/Months.js
@@ -1,2 +1,18 @@
-import React from 'react' 
+import React from 'react'
+import {Route, Link} from 'react-router-dom'
+import Account from './Account'
 
+const Months = (props) => {
+
+  return (
+    <div>
+      {props.months.map(month =>
+        <li key={month.id}>
+          <Link to={`/months/${month.id}`}>{month.name}</Link>
+        </li> )}
+    </div>
+
+  )
+}
+
+export default Months
