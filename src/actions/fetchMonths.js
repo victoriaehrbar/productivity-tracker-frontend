f59715d0dd2e2efd8de0703c48ad863b723a5b4a
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-07 18:26:32 -0600

    changed month fetch

--- src/actions/fetchMonths.js
+++ src/actions/fetchMonths.js
@@ -1,10 +1,9 @@
 export function fetchMonths() {
     return (dispatch) => {
-      fetch('http://localhost:3000/api/v1/months')
-      .then(resp => resp.json())
-      .then(months => dispatch({
-        type: 'FETCH_MONTHS',
-        payload: months
-      }))
+      fetch("http://localhost:3000/api/v1/months")
+      .then(response => response.json())
+      .then(json => {
+          dispatch({type: "FETCH_MONTHS", payload: json.data})
+        })
+      }
     }
-  }
